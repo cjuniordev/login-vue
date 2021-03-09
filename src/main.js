e947aaee225382e8c6e46d2b3f8d1d0fbca6635c
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-09 15:19:40 -0300

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 
+// font awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faExclamationCircle);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 import App from './App';
 import router from './router';
 import store from './store';
